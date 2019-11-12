44c5acad16e6e1bb506cecf7bbad08a4d24d6010
Guillaume Isabelle <jgi@jgwill.com>
2019-11-12 12:49:44 -0500

    @STCGoal Automated publication of my rendering into a VR asset for Web app consumption -> Self/collaborator Analysis @STCIssue I am exporting a jpeg sequence of my render into this repo and steps to have a post created are taking unacceptable amount of time / amount of operations

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+/*
+ */
+//@STCGoal Automated publication of my rendering into a VR asset for Web app consumption -> Self/collaborator Analysis
+/*
+git add x__raining__191109__03__Rigging_take2/ &&\
+git commit x__raining__191109__03__Rigging_take2/ \
+-m "x__raining__191109__03__Rigging_take2
+Experimentation of Particle networking - result are rain drop, splashing and sliding streams.  by Guillaume Descoteaux-Isabelle 19-11-12
+http://guillaumeisabelle.com/nad-previz/2019/11/12/assets-vr-1909-x__raining__191109__03__rigging_take2/" \
+&& git push \
+&& ssh root@py "cd /gi/wp-content/uploads/sites/assets-vr-1909&& git pull && chmod -R 777 ."
+*/
+//@STCIssue I am exporting a jpeg sequence of my render into this repo and steps to have a post created are taking unacceptable amount of time / amount of operations
+
